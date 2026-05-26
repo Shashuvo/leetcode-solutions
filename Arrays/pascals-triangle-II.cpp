b1a2edc7c86d29e6986718a7c87279c8a5f89606
Shashuvo <shahariat2810@gmail.com>
2026-05-26 21:51:23 +0600

    pascal's triangle II solved

---
+++ Arrays/pascals-triangle-II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row;
+            long long ans = 1;
+            row.push_back(ans);
+            for (int j = 1; j <= i; j++) {
+                ans = ans * (i - j + 1);
+                ans = ans / j;
+                row.push_back(ans);
+            }
+            triangle.push_back(row);
+        }
+
+        return triangle;
+    }
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> triangle = generate(rowIndex + 1);
+        return triangle[rowIndex];
+    }
+};
